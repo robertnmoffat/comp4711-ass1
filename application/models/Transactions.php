3fb04913b2ffa57cba8d7837b6e143d2d18e4b0c
Robert <robertnmoffat@gmail.com>
2016-02-13 12:30:11 -0800

    got current holdings working

--- application/models/Transactions.php
+++ application/models/Transactions.php
@@ -23,13 +23,17 @@ class Transactions extends CI_Model {
         }
         
         function amountByPlayer($pname){
-                //$this->db->order_by("Stock");
-                $this->db->select('Stock, SUM(Quantity) AS total', FALSE);
-                $checkName = "Player = '{$pname}' AND Trans = 'buy'";
+                $this->db->order_by("Stock");
+                $this->db->select("Stock, SUM(CASE
+               WHEN Trans = 'buy' THEN Quantity
+               ELSE Quantity * -1
+            END) AS total", FALSE);
+                $checkName = "Player = '{$pname}'";
                 $this->db->where($checkName);
                 $this->db->group_by('Stock');
 		$query = $this->db->get('transactions');
-		return $query->result_array();            
+                
+                return $query->result_array();
         }
 	
 }
