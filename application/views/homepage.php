08343095bc923e0bc3c84b8403ed40c5c0b7fdf1
Brian Livesey <bri@brianlivesey.com>
2016-02-12 23:33:38 -0800

    Working homepage with formatting and login

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,7 +1,8 @@
 <h1>Stock T-t-t-ticker!!</h1>
 <h3>Money is the root of all evil</h3>
-</br></br><p>
-<table>
+<p>Welcome, {username}!
+    
+<p><table cellspacing = 25>
 <tr><td>
 Stocks currently trading:
 </td><td>
@@ -13,3 +14,5 @@ Active players:
 {playersList}
 </td></tr>
 </table>
+
+<p><a href="/welcome/logout">Log out</a>
